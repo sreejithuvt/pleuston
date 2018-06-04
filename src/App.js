7bfb66a04b00472cf77cb3aa68824ecee165e636
Matthias Kretschmann <m@kretschmann.io>
2018-06-04 16:21:15 +0200

    add a router and some pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import Routes from './Routes'
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img alt="logo" className="App-logo" src={logo} />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        )
-    }
-}
+const App = () => (
+    <div className="app">
+        <Routes />
+    </div>
+)
 
 export default App
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => (
+    <main className="screen screen--dashboard">
+        <h1>Dashboard</h1>
+    </main>
+)
+
+export default Dashboard
