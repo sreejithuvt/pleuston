43fb1c4f500b1e20571758ece64f3c5cc607f48b
Matthias Kretschmann <m@kretschmann.io>
2018-06-07 12:25:13 +0200

    unify box styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,9 @@ const Header = ({ activeAccount }) => (
     <header className="header">
         <a className="header__logo" href="/">
             <img alt="logo" className="header__logo__image" src={logo} />
+            <h1 className="header__title">
+                Pigeon market
+            </h1>
         </a>
         <nav className="header__menu">
             <span className="header__menu__user">{activeAccount ? activeAccount.name : 'No account selected'}</span>
