c127bdaa3312dfd610c48afd6119b928fc724812
Matthias Kretschmann <m@kretschmann.io>
2018-06-07 12:40:38 +0200

    header tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ReactModal from 'react-modal'
 
+import Wallet from '../components/Wallet'
 import logo from '../../node_modules/oceanprotocol-art/logo/logo-white.svg'
 import './Header.css'
 
-const Header = ({ activeAccount }) => (
-    <header className="header">
-        <a className="header__logo" href="/">
-            <img alt="logo" className="header__logo__image" src={logo} />
-            <h1 className="header__title">
-                Pigeon market
-            </h1>
-        </a>
-        <nav className="header__menu">
-            <span className="header__menu__user">{activeAccount ? activeAccount.name : 'No account selected'}</span>
-        </nav>
-    </header>
-)
+class Header extends Component {
+    constructor() {
+        super()
+        this.state = {
+            showModal: false
+        }
+
+        this.handleOpenModal = this.handleOpenModal.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this)
+    }
+
+    handleOpenModal() {
+        this.setState({ showModal: true })
+    }
+
+    handleCloseModal() {
+        this.setState({ showModal: false })
+    }
+
+    render() {
+        const { activeAccount } = this.props
+
+        return (
+            <header className="header">
+                <a className="header__logo" href="/">
+                    <img alt="logo" className="header__logo__image" src={logo} />
+                    <h1 className="header__title">
+                        Pigeon market
+                    </h1>
+                </a>
+                <nav className="header__menu" onClick={this.handleOpenModal}> {/* eslint-disable-line */}
+                    <span className="header__menu__user" title={activeAccount ? activeAccount.name : null}>
+                        {activeAccount ? `${activeAccount.name.slice(0, 20)}...` : 'No account selected'}
+                    </span>
+                    <span className="header__menu__balance">{activeAccount ? activeAccount.balance : null}</span>
+                </nav>
+
+                <ReactModal
+                    contentLabel="Minimal Modal Example"
+                    isOpen={this.state.showModal}>
+                    <Wallet />
+                    <button onClick={this.handleCloseModal}>Close Modal</button>
+                </ReactModal>
+            </header>
+        )
+    }
+}
 
 Header.propTypes = {
     activeAccount: PropTypes.object, // eslint-disable-line
