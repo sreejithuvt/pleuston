f72c8c16ef68ba113a3416ed31018ab63cff194e
diminator <dimi.dejonghe@gmail.com>
2018-06-11 13:38:13 +0200

    assetlistloader

---
+++ src/containers/AssetListLoader.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux'
+
+import AssetList from '../components/AssetList'
+
+
+export default connect(state => ({
+    ...state.account
+}))(AssetList)
--- src/pages/Datasets.js
+++ src/pages/Datasets.js
@@ -1,23 +1,20 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-import Header from '../components/Header'
 import ScreenHeader from '../components/ScreenHeader'
-import AssetList from '../components/AssetList'
+import HeaderLoader from '../containers/HeaderLoader'
+import AssetListLoader from '../containers/AssetListLoader'
 
-const Datasets = props => {
-    const { activeAccount } = props.location.state
+const Datasets = () => (
+    <Fragment>
+        <HeaderLoader />
+        <main className="screen screen--datasets">
+            <ScreenHeader title="Datasets" />
+            <AssetListLoader />
+        </main>
+    </Fragment>
+)
 
-    return (
-        <Fragment>
-            <Header activeAccount={activeAccount} />
-            <main className="screen screen--datasets">
-                <ScreenHeader title="Datasets" />
-                <AssetList activeAccount={activeAccount} />
-            </main>
-        </Fragment>
-    )
-}
 
 Datasets.propTypes = {
     location: PropTypes.object.isRequired, // eslint-disable-line
