a7f9013ad292bfba65091ffa932ca62b2836ac8c
Matthias Kretschmann <m@kretschmann.io>
2018-07-23 14:19:59 +0200

    lint fix

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ module.exports = {
     keeperScheme: 'http',
 
     // Setup with local services
-//    keeperHost: 'localhost',
-//    keeperPort: 8545,
-//    dbScheme: 'http',
-//    dbHost: 'localhost',
-//    dbHeaders: {},
-//    dbPort: 9984,
+    // keeperHost: 'localhost',
+    // keeperPort: 8545,
+    // dbScheme: 'http',
+    // dbHost: 'localhost',
+    // dbHeaders: {},
+    // dbPort: 9984,
 
     // Setup using remote services
     // private ocean test net @40.115.16.244
@@ -18,8 +18,8 @@ module.exports = {
     dbHost: 'test.bigchaindb.com',
     dbHeaders: {
         app_id: '54ed26dd', // Can get a new one from testnet.bigchaindb.com
-        app_key: 'd068996d8d5b1a66cfc61dc3a83fa7ee'}, // Can get a new one from testnet.bigchaindb.com
-
+        app_key: 'd068996d8d5b1a66cfc61dc3a83fa7ee'
+    }, // Can get a new one from testnet.bigchaindb.com
 
     dbNamespace: 'ocean:plankton:v0.0.0:asset:metadata',
 }
