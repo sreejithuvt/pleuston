409ef99173b8a837db79877e12a7ea988e25f0f6
Matthias Kretschmann <m@kretschmann.io>
2018-07-25 15:46:09 +0200

    basic new layout

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => (
+    <nav className="layout__sidebar">
+        <ul className="sidebar">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/datasets/new">Publish Data</a>
+            </li>
+            <li>
+                <a href="/datasets">My Data</a>
+            </li>
+            <li>
+                <a href="/orders">Orders</a>
+            </li>
+            <li>
+                <a href="/history">History</a>
+            </li>
+        </ul>
+    </nav>
+)
+
+export default Sidebar
