8a6c63655a42b3a4c3bb69e6a57e92c67c692ebd
Matthias Kretschmann <m@kretschmann.io>
2018-08-07 13:35:44 +0200

    manually move bigchaindb drivers into create-react-app src folder

---
+++ src/lib/bigchaindb-orm/connection.js
@@ -0,0 +1,119 @@
+import * as driver from '../bigchaindb-driver' // eslint-disable-line import/no-namespace
+
+export default class Connection {
+    constructor(path, headers = {}) {
+        this.path = path
+        this.headers = Object.assign({}, headers)
+        this.conn = new driver.Connection(path, headers)
+    }
+
+    getAssetId(tx) { // eslint-disable-line class-methods-use-this
+        return tx.operation === 'CREATE' ? tx.id : tx.asset.id
+    }
+
+    getTransaction(transactionId) {
+        return this.conn.getTransaction(transactionId)
+    }
+
+    listTransactions(assetId, operation) {
+        return this.conn.listTransactions(assetId, operation)
+    }
+
+    listOutputs(publicKey, spent) {
+        return this.conn.listOutputs(publicKey, spent)
+    }
+
+    getBlock(blockId) {
+        return this.conn.getBlock(blockId)
+    }
+
+    listBlocks(transactionId) {
+        return this.conn.listBlocks(transactionId)
+            .then(blockIds => Promise.all(blockIds.map(blockId => this.conn.getBlock(blockId))))
+    }
+
+    listVotes(blockId) {
+        return this.conn.listVotes(blockId)
+    }
+
+    searchAssets(text) {
+        return this.conn.searchAssets(text)
+    }
+
+    createTransaction(publicKey, privateKey, payload, metadata) {
+        try {
+            // Create a transation
+            const tx = driver.Transaction.makeCreateTransaction(
+                payload,
+                metadata,
+                [
+                    driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(publicKey))
+                ],
+                publicKey
+            )
+
+            // sign/fulfill the transaction
+            const txSigned = driver.Transaction.signTransaction(tx, privateKey)
+            return this.conn.postTransactionCommit(txSigned).then(() => txSigned)
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    }
+
+    transferTransaction(tx, fromPublicKey, fromPrivateKey, toPublicKey, metadata) {
+        try {
+            const txTransfer = driver.Transaction.makeTransferTransaction(
+                [{ 'tx': tx, 'output_index': 0 }],
+                [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(toPublicKey))],
+                metadata,
+            )
+            const txTransferSigned = driver.Transaction.signTransaction(txTransfer, fromPrivateKey)
+            // send it off to BigchainDB
+            return this.conn.postTransactionCommit(txTransferSigned).then(() => txTransferSigned)
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    }
+
+    getSortedTransactions(assetId) {
+        return this.conn.listTransactions(assetId)
+            .then((txList) => {
+                if (txList.length <= 1) {
+                    return txList
+                }
+                const inputTransactions = []
+                txList.forEach((tx) =>
+                    tx.inputs.forEach(input => {
+                        if (input.fulfills) {
+                            inputTransactions.push(input.fulfills.transaction_id)
+                        }
+                    }))
+                const unspents = txList.filter((tx) => inputTransactions.indexOf(tx.id) === -1)
+                if (unspents.length) {
+                    let tipTransaction = unspents[0]
+                    let tipTransactionId = tipTransaction.inputs[0].fulfills.transaction_id
+                    const sortedTxList = []
+                    while (true) { // eslint-disable-line no-constant-condition
+                        sortedTxList.push(tipTransaction)
+                        try {
+                            tipTransactionId = tipTransaction.inputs[0].fulfills.transaction_id
+                        } catch (e) {
+                            break
+                        }
+                        if (!tipTransactionId) {
+                            break
+                        }
+                        tipTransaction = txList.filter((tx) => // eslint-disable-line no-loop-func, prefer-destructuring
+                            tx.id === tipTransactionId)[0]
+                    }
+                    return sortedTxList.reverse()
+                } else {
+                    console.error(
+                        'something went wrong while sorting transactions',
+                        txList, inputTransactions
+                    )
+                }
+                return txList
+            })
+    }
+}
