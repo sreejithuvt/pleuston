72e26a9eb796ab617530001abb4719cd00535ffc
Matthias Kretschmann <m@kretschmann.io>
2018-08-09 17:20:41 +0200

    switch to a light, minimal form

--- src/components/asset/AssetNew.js
+++ src/components/asset/AssetNew.js
@@ -12,20 +12,23 @@ const AssetNew = ({
 }) => (
     <form className="form" onSubmit={handleSubmit}>
         <div className="form__group">
+            <h1 className="form__title">Publish a new asset</h1>
+        </div>
+        <div className="form__group">
             <FormLabel htmlFor="name" label="Title" />
-            <FormInput required component="input" name="name" type="text" placeholder="The title of your asset" />
+            <FormInput required component="input" name="name" id="name" type="text" placeholder="The title of your asset" />
         </div>
         <div className="form__group">
             <FormLabel htmlFor="description" label="Abstract" />
-            <FormInput required component="textarea" name="description" placeholder="Describe your asset" />
+            <FormInput required component="textarea" name="description" id="description" placeholder="Describe your asset" />
         </div>
         <div className="form__group">
             <FormLabel htmlFor="url" label="Url" />
-            <FormInput required component="input" name="url" type="url" placeholder="https://url.com/" />
+            <FormInput required component="input" name="url" id="url" type="url" placeholder="https://url.com/" />
         </div>
         <div className="form__group">
             <FormLabel htmlFor="price" label="Price" />
-            <FormInput required type="number" component="input" name="price" placeholder="Price in ocean tokens" />
+            <FormInput required type="number" component="input" name="price" id="price" placeholder="Price in ocean tokens" />
         </div>
         <div className="form__group">
             <Button primary="true" type="submit">Publish</Button>
