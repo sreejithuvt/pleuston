34916a4a53c9f5d654eda48fa1376713026ae185
Matthias Kretschmann <m@kretschmann.io>
2018-08-10 18:28:24 +0200

    move assets filter into assets list component

--- src/pages/Datasets.js
+++ src/pages/Datasets.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import ScreenHeader from '../components/ScreenHeader'
-
-import AssetFilterLoader from '../containers/AssetFilterLoader'
 import AssetListLoader from '../containers/AssetListLoader'
 
 const Datasets = () => (
     <Layout>
         <ScreenHeader title="Data Sets" subtitle="See all your data sets" />
-        <AssetFilterLoader />
         <AssetListLoader />
     </Layout>
 )
