b341dc6a0198f0c40ba68f2eb55d70b923c44f03
Matthias Kretschmann <m@kretschmann.io>
2018-09-10 13:07:58 +0200

    fix Docker config path

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 envsubst '${KEEPER_SCHEME} ${KEEPER_HOST} ${KEEPER_PORT} ${OCEAN_SCHEME} ${OCEAN_HOST} ${OCEAN_PORT}'\
-  < ${DIR}/../src/config.js.template > $DIR/../src/config.js
+  < ${DIR}/src/config/config.js.template > $DIR/src/config/config.js
 sleep 30
 npm run build
-serve -l tcp://${LISTEN_ADDRESS}:${LISTEN_PORT} -s ${DIR}/../build/
+serve -l tcp://${LISTEN_ADDRESS}:${LISTEN_PORT} -s ${DIR}/build/
