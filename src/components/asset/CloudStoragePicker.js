ca4daae58657332e5465bbc1bac5dd91e5a1a995
Matthias Kretschmann <m@kretschmann.io>
2018-09-13 19:41:17 +0200

    file picker prototype

--- src/components/asset/CloudStoragePicker.js
+++ src/components/asset/CloudStoragePicker.js
@@ -1,22 +1,64 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import MultiSelectReact from 'multi-select-react'
+// import MultiSelectReact from 'multi-select-react'
 
 import styles from './CloudStoragePicker.module.scss'
 
-const CloudStoragePicker = ({ blobs, urlGetter }) => (
-    <MultiSelectReact
-        className={styles.select}
-        name="selectBlobs"
-        options={blobs}
-        selectedBadgeClicked={urlGetter}
-        isSingleSelect
-    />
-)
+export default class CloudStoragePicker extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selection: null
+        }
+    }
+    handleSelection(e) {
+        if (e.currentTarget.class === styles.selected) {
+            e.currentTarget.className = styles.file
+        } else {
+            e.currentTarget.className = styles.selected
+        }
+
+        if (this.state.selection === e.currentTarget) this.props.urlGetter()
+
+        this.setState({ selection: e.currentTarget })
+    }
+
+    render() {
+        const { blobs } = this.props
+
+        return (
+            <div className={styles.files}>
+                {
+                    blobs === undefined || blobs.length === 0 ? (
+                        <div className={styles.empty}>
+                            <span>No files found</span>
+                        </div>
+                    ) : (
+                        blobs.map(blob => (
+                            <span
+                                key={blob}
+                                onClick={(e) => this.handleSelection(e)}
+                                className={styles.file}
+                            >
+                                {blob}
+                            </span>
+                        ))
+                    )
+                }
+            </div>
+            // <MultiSelectReact
+            //     className={styles.select}
+            //     name="selectBlobs"
+            //     options={this.props.blobs}
+            //     selectedBadgeClicked={this.props.urlGetter}
+            //     isSingleSelect
+            // />
+        )
+    }
+}
 
 CloudStoragePicker.propTypes = {
     urlGetter: PropTypes.func.isRequired,
     blobs: PropTypes.array.isRequired
 }
-
-export default CloudStoragePicker
