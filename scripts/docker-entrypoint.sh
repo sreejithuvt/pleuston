379528543021cd8eac14851e07f4e6076f3d4b2e
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-14 11:11:04 +0200

    Refactor to avoid codacy workaround

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-envsubst '${KEEPER_SCHEME} ${KEEPER_HOST} ${KEEPER_PORT} ${OCEAN_SCHEME} ${OCEAN_HOST} ${OCEAN_PORT}'\
-  < /pleuston/config/config.js.template > /pleuston/config/config.js
+envsubst < /pleuston/config/config.js.template > /pleuston/config/config.js
 echo "Starting Pleuston..."
 npm run build
 serve -l tcp://"${LISTEN_ADDRESS}":"${LISTEN_PORT}" -s /pleuston/build/
