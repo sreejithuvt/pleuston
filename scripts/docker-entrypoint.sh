c92f892aebcf87f23150b397ce27c4b20fa10f54
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-14 10:35:01 +0200

    Fixed building process inside Docker image

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-envsubst "${KEEPER_SCHEME} ${KEEPER_HOST} ${KEEPER_PORT} ${OCEAN_SCHEME} ${OCEAN_HOST} ${OCEAN_PORT}"\
+envsubst '${KEEPER_SCHEME} ${KEEPER_HOST} ${KEEPER_PORT} ${OCEAN_SCHEME} ${OCEAN_HOST} ${OCEAN_PORT}'\
   < /pleuston/config/config.js.template > /pleuston/config/config.js
-sleep 30
+echo "Starting Pleuston..."
 npm run build
 serve -l tcp://"${LISTEN_ADDRESS}":"${LISTEN_PORT}" -s /pleuston/build/
