ecbf37346c1315f5f0783ea04ae24aab06d12dea
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-19 12:39:29 +0200

    Wait for flag from keeper-contracts when contracts are ready

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 envsubst < /pleuston/config/config.js.template > /pleuston/config/config.js
-echo "Waiting for keeper-contracts ${KEEPER_CONTRACTS_WAIT_SLEEP:-60} seconds..."
-sleep ${KEEPER_CONTRACTS_WAIT_SLEEP:-60}
+echo "Waiting for contracts to be generated..."
+while [ ! -f "/pleuston/node_modules/@oceanprotocol/keeper-contracts/artifacts/ready" ]; do
+  sleep 2
+done
 echo "Starting Pleuston..."
 npm run build
 serve -l tcp://"${LISTEN_ADDRESS}":"${LISTEN_PORT}" -s /pleuston/build/
