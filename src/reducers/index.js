be5ac2d1b3868471ad094866fa94f4fb0ca6f195
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-09-20 11:10:56 +0200

    no direct access to contracts allowed anymore

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import { reducer as formReducer } from 'redux-form'
 
 import account from './account'
 import asset from './asset'
-import contract from './contract'
 import provider from './provider'
 import order from './order'
 
@@ -11,7 +10,6 @@ const appReducer = combineReducers({
     form: formReducer,
     account,
     asset,
-    contract,
     provider,
     order
 })
