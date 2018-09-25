8974a39513a949b17c7bf94bbd903315a24a5ba3
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-09-25 10:25:41 +0200

    support both versions of squid with either async constructor or getInstance

--- src/actions/ocean.js
+++ src/actions/ocean.js
@@ -15,13 +15,15 @@ export async function provideOcean() {
     const nodeUri = `${keeperScheme}://${keeperHost}:${keeperPort}`
     const providerUri = `${providerScheme}://${providerHost}:${providerPort}/api/v1/provider`
 
-    const ocean = await new Ocean({
+    const config = {
         // todo: change this when the new interface of metmask is released
         web3Provider: global.web3 ? global.web3.currentProvider : null,
         // this is just a fallback in case web3 is not injected
         nodeUri,
         providerUri
-    })
+    }
+
+    const ocean = Ocean.getInstance ? await Ocean.getInstance(config) : await new Ocean(config)
 
     return { ocean }
 }
